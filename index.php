d32e29685440613cbbdd6e2159386f96b5554c95
LivingtonMerello98 <livington.merello@gmail.com>
2024-06-26 17:11:58 +0200

    [upd] set product and category

--- index.php
+++ index.php
@@ -1,1 +1,40 @@
 
+<?php
+
+class Product
+{
+    private string $title;
+    private int $price;
+    private string $image;
+    private Category $category;
+
+    //costruttore
+    public function __construct($_image, $_title, $_price, Category $_category)
+    {
+        $this->image = $_image;
+        $this->title = $_title;
+        $this->price = $_price;
+        $this->category = $_category;
+    }
+
+    //funzione per stampare
+    public function printProduct()
+    {
+        echo 'image: ' . $this->image . ' ';
+        echo 'title: ' . $this->title . ' ';
+        echo 'price: ' . $this->price . ' ';
+        echo 'category: ' . $this->category . ' ';
+    }
+}
+
+
+class Category
+{
+    protected $name;
+    protected $icon;
+}
+
+
+//debug
+$product1 = new Product('product_image', 'Product 1', 10, new Category('Category 1', 'product_image'));
+echo $product1->printProduct();
